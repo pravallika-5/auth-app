a083e07fffed3f336bd60ada4c327cf2761ae6a2
pravallika-5 <146409792+pravallika-5@users.noreply.github.com>
2025-07-26 15:41:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const cors = require("cors");
 
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://auth-app-front-c4pt8rmhz-pravallikas-projects-d20da3f3.vercel.app"
+}));
 app.use(express.json());
 
 
