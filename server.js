44cc638760ddc6ca9912d5b613589cae94cd308e
pravallika-5 <146409792+pravallika-5@users.noreply.github.com>
2025-07-26 18:27:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(cors({
-  origin: "https://auth-app-front-pied.vercel.app/",
+  origin: "https://auth-app-front-pied.vercel.app",
    credentials: true 
 }));
 app.use(express.json());
